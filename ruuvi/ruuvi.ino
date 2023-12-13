aece5376740616c87b1ca4f89a3e5faa7a679a77
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-13 14:13:21 +0200

    Send room temperature to sql database every hour

--- ruuvi/ruuvi.ino
+++ ruuvi/ruuvi.ino
@@ -1,12 +1,14 @@
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEScan.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 #include "secrets.h"
 
-
 BLEAddress ruuviTagAddress(ruuviTagMacAddress);
-
 BLEScan* pBLEScan;
+
+const char* serverUrl = "http://reetuinkila.eu.pythonanywhere.com/add";
 
 //Converts hexadecimal values to decimal values
 int hexadecimalToDecimal(String hexVal)
@@ -37,31 +39,51 @@ int hexadecimalToDecimal(String hexVal)
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     if (advertisedDevice.getAddress() == ruuviTagAddress) {
-      Serial.println("RuuviTag found!");
       String raw_data = String(BLEUtils::buildHexData(nullptr, (uint8_t*)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length()));
       raw_data.toUpperCase();
-      Serial.println(raw_data);
-      Serial.println(raw_data.substring(4, 6));
-      Serial.println(hexadecimalToDecimal(raw_data.substring(6, 10))*0.005);
-      Serial.println(hexadecimalToDecimal(raw_data.substring(10, 14))*0.0025);
-      Serial.println(hexadecimalToDecimal(raw_data.substring(14, 18))*1+50000);
-
+      float temp = hexadecimalToDecimal(raw_data.substring(6, 10))*0.005;
+      float hum = hexadecimalToDecimal(raw_data.substring(10, 14))*0.0025;
+      int pres = hexadecimalToDecimal(raw_data.substring(14, 18))*1+50000;
+      sendData(temp, hum, pres);
     }
   }
 };
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("ESP32 RuuviTag Scanner");
-
   BLEDevice::init("");
   pBLEScan = BLEDevice::getScan(); 
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
 }
 
 void loop() {
-  Serial.println("Scanning...");
   pBLEScan->start(5, false);
+  delay(60000*60); // Odota 60 minuuttia ennen seuraavaa skannausta
+}
 
-  delay(60000); // Odota minuutti ennen seuraavaa skannausta
+void sendData(float temp, float hum, int pres){
+    // Connect to Wi-Fi
+    WiFi.begin(SSID, PASS);
+    Serial.println("Connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    WiFiClient client;
+    HTTPClient http;
+
+    http.begin(client, serverUrl);
+    http.addHeader("Content-Type", "text/plain");
+
+    String postData = String(temp) + "," +
+                      String(hum) + "," +
+                      String(pres) + "," +
+                      SQL_pswd;
+
+    Serial.println(postData);
+    
+    int httpResponseCode = http.POST(postData);
+    Serial.println(httpResponseCode);
+    http.end();
 }
