788ec763bea3aac6016ddbf0673d08524800dc61
lrtinkiv <lrtinkiv@jyu.fi>
2023-12-13 08:30:39 +0200

    tietokantayhteys lisätty

--- web app/app.py
+++ web app/app.py
@@ -1,6 +1,28 @@
+import io
 from flask import Flask
- 
+import json
+import mysql.connector
+import mysql.connector.pooling
+import mysql.connector.errors
+from mysql.connector import errorcode
+
 app = Flask(__name__)
+
+# Tietokantayhteys
+tiedosto = io.open("/home/reetuinkila/saa/dbconfig.json", encoding="UTF-8")
+dbconfig = json.load(tiedosto)
+try:
+    pool=mysql.connector.pooling.MySQLConnectionPool(pool_name="tietokantayhteydet",
+    pool_size=2, #PythonAnywheren ilmaisen tunnuksen maksimi on kolme
+    **dbconfig
+    ) 
+except mysql.connector.Error:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Tunnus tai salasana on väärin")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Tietokantaa ei löydy")
+  else:
+    print(err)
  
 @app.route('/')
 def hello_world():
@@ -8,4 +30,3 @@ def hello_world():
  
 if __name__ == '__main__':
     app.run()
-
